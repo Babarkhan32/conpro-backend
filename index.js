15b928745757ab2807f6d13be3f620cfa69f4627
babarkhan32 <babar.0khan1@gmail.com>
2023-02-19 12:23:19 +0500

    added cors

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 4001;
 const router = require("./routes/index");
 require("./config/dbConfig");
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
