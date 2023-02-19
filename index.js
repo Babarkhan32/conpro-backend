df00cb91301461042512f873d774c18bb7553d64
babarkhan32 <babar.0khan1@gmail.com>
2023-02-19 12:20:21 +0500

    pushed mongo db connection

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const port = 4001;
 const router = require("./routes/index");
-app.use(
-    express.json()
-)
+require("./config/dbConfig");
+app.use(express.json());
 app.use("/api", router);
 
 app.listen(port, () => {
